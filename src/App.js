f63314407b4fff50cc6e71abbaec5bc9fb9b39c6
gavin-crowley <42537387+gavin-crowley@users.noreply.github.com>
2020-03-07 10:08:30 +0400

    2-form-added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import Contacts from './components/contacts/Contacts';
 import Header from './components/layout/Header';
+import AddContact from './components/contacts/AddContact';
+
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
@@ -11,6 +13,7 @@ class App extends Component {
       <div className="App">
         <Header branding="Contact Manager" />
         <div className="container">
+          <AddContact />
           <Contacts />
         </div>
       </div>
